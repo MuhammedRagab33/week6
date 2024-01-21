5b4aac290ea2eae4ec16e26159a69046d317613a
MuhammedRagab33 <mr6747477@gmail.com>
2024-01-21 22:18:50 +0200

    First commit

---
+++ js/min.js
@@ -0,0 +1,508 @@
+/////////////1////////////////
+
+// var x = window.prompt() 
+// console.log(x)
+
+
+///////////////2///////////////////
+
+
+// var x = window.prompt()
+// var z = Number(x) % Number(3) 
+// if (z == "0"){
+//     console.log ("yes");}
+// else{
+// console.log("no");
+// }
+
+
+///////////////3//////////////////
+
+
+// var x = Number(window.prompt())
+// var y = Number(window.prompt())
+// if(x > y){
+//     console.log(x);}
+// else if(y > x){
+//     console.log(y);
+// }
+
+
+//////////////4//////////////
+
+
+// var x = Number(window.prompt())
+// if (x < 0){
+//     console.log("negative");}
+// else{
+// console.log("positive")
+// }
+
+
+///////////////5////////////////
+
+
+// var x = Number(window.prompt())
+// var y = Number(window.prompt())
+// var z = Number(window.prompt())
+// if(x<y,x<z)
+// {console.log(x)}
+// else if(y<x,y<z)
+// {console.log(y)}
+// else if(z<x,z<y)
+// {console.log(z)}
+// if(x>y,x>z)
+// {console.log(x)}
+// else if(y>x,y>z)
+// {console.log(y)} 
+// else if(z>x,z>y)
+// {console.log(z)} 
+
+
+///////////////6///////////////
+
+
+// var x = Number(window.prompt())
+// if (x %2 ==0){
+//     console.log("even")
+// }else{
+//     console.log("odd")
+// }
+
+
+///////////////7////////////////
+
+
+// var x = window.prompt()
+// var y = ("a")
+// var z = ("e")
+// var m = ("i")
+// var n = ("o")
+// var o = ("u")
+// if (x ==y){
+//     console.log("vowel")
+// }
+// else if (x ==z){
+//     console.log("vowel")
+// }
+// else if (x ==m){
+//     console.log("vowel")
+// }
+// else if (x ==n){
+//     console.log("vowel")
+// }
+// else if (x ==o){
+//     console.log("vowel")
+// }
+// else{
+//     console.log("consonant")
+// }
+
+
+//////////////8////////////////
+
+
+// var x = window.prompt()
+// for  (i = x; i >=1; i--) 
+// console.log(i)
+
+
+//////////////9///////////////
+
+
+// var x = Number(window.prompt())
+// var y = x * 1
+// console.log(y)
+// var y = x * 2
+// console.log(y)
+// var y = x * 3
+// console.log(y)
+// var y = x * 4
+// console.log(y)
+// var y = x * 5
+// console.log(y)
+// var y = x * 6
+// console.log(y)
+// var y = x * 7
+// console.log(y)
+// var y = x * 8
+// console.log(y)
+// var y = x * 9
+// console.log(y)
+// var y = x * 10
+// console.log(y)
+// var y = x * 11
+// console.log(y)
+// var y = x * 12
+// console.log(y)
+
+
+//////////////10////////////
+
+
+// var x = Number(window.prompt())
+// for(i=0; i<=x; i++){
+//     if(i%2==0){ console.log(i)}
+// }
+
+
+
+//////////////11/////////////
+
+
+// var x = Number(window.prompt())
+// var y = Number(window.prompt())
+// console.log(x**y)
+
+
+//////////////12//////////////
+
+
+// var x = Number(window.prompt())
+// if (x){
+// console.log(x)}
+// var a = Number(window.prompt())
+// if (a){
+// console.log(a)}
+// var b = Number(window.prompt())
+// if (b){
+// console.log(b)}
+// var c = Number(window.prompt())
+// if (c){
+// console.log(c)}
+// var d = Number(window.prompt())
+// if (d){
+// console.log(d)}
+// var m = x+a+b+c+d
+// if(m){console.log(m)}
+// var z = (m /5)
+// if(z){console.log(z)}
+// var u = (m /5)
+// if(u){console.log(u)
+// }
+
+
+//////////////13////////////
+
+
+// var x = window.prompt()
+// var a = ("1")
+// var b = ("2")
+// var c = ("3")
+// var d = ("4")
+// var e = ("5")
+// var f = ("6")
+// var g = ("7")
+// var h = ("8")
+// var n = ("9")
+// var o = ("10")
+// var y = ("11")
+// var z = ("12")
+// if (x ==a){
+//     console.log("31")
+// }
+// else if (x ==b){
+//     console.log("28")
+// }
+// else if (x ==c){
+//     console.log("31")
+// }
+// else if (x ==d){
+//     console.log("30")
+// }
+// else if (x ==e){
+//     console.log("30")
+// }
+// if (x ==f){
+//     console.log("31")
+// }
+// else if (x ==g){
+//     console.log("30")
+// }
+// else if (x ==h){
+//     console.log("31")
+// }
+// else if (x ==h){
+//     console.log("31")
+// }
+// else if (x ==o){
+//     console.log("30")
+// }
+// if (x ==y){
+//     console.log("31")
+// }
+// else if (x ==z){
+//     console.log("30")
+// }
+
+
+///////////////14///////////
+
+
+// var x = window.prompt()
+// var y =("physics")
+// if (x ==y){
+// console.log("90%")
+// }
+// var y =("chemistry")
+// if (x ==y){
+// console.log("80%")
+// }
+// var y =("biology")
+// if (x ==y){
+// console.log("70%")
+// }
+// var y =("mathematics")
+// if (x ==y){
+// console.log("60%")
+// }
+// var y =("computer")
+// if (x ==y){
+// console.log("40%")
+// }
+
+
+///////////////15/////////////
+
+
+// var x = ""
+// for(var i = 30 ; i >= 1; i--){
+//     x +="<option>"+ i+ "</option>"
+// }
+// console.log(x)
+
+
+/////////////////16////////////////
+
+
+// var x = window.prompt()
+// switch(x){ 
+//     case 'a':
+//     console.log("vawel")
+//     break;
+//     case 'e':
+//     console.log("vawel")
+//     break;
+//     case 'i':
+//     console.log("vawel")
+//     break;
+//     case 'o':
+//     console.log("vawel")
+//     break;
+//     case 'u':
+//     console.log("vawel")
+//     break;
+//     default:
+//     console.log("consonant")
+// }
+
+
+///////////////17 switch/////////////
+
+
+
+// var x = Number(window.prompt())
+// var y = Number(window.prompt())
+// switch(x>y){
+// default:console.log(x)
+// switch(y<x){
+// default:console.log(y)
+// }
+// }
+
+
+///////////////18 switch//////////////
+
+
+// var x = Number(window.prompt())
+// switch (x %2 == 0){
+// default:console.log("even")
+// break;
+// default:console.log("odd")
+// }
+
+
+///////////19///////////////
+
+
+
+// var x = Number(window.prompt())
+// if (x < 0){
+//     console.log("negative");}
+// else if(x>0){
+// console.log("positive")
+// }
+// else{
+//     console.log("zero")
+// }
+
+
+///////////////20///////////////
+
+
+// var x = Number(window.prompt())
+// var y = Number(window.prompt())
+// var z = y * x
+// switch(Number(z)){
+//     default:console.log(z)
+// }
+
+
+
+///////////////////17 if///////
+
+
+// var y = Number(window.prompt())
+// var x = Number(window.prompt())
+// if(y > x){
+//     console.log(x);}
+// else if(x > y){
+//     console.log(y);
+// }
+
+
+//////////////18 if//////////////
+
+// var z = Number(window.prompt())
+// if (z %2 ==0){
+//     console.log("even")
+// }else{
+//     console.log("odd")
+// }
+///////////////////////////////////////////////////////////////////////////////////
+
+
+
+// var productNameInput=document.getElementById('prodName')
+// var productCategoryInput=document.getElementById('prodCategory')
+// var productPriceInput=document.getElementById('prodPricr')
+// var productDescInput=document.getElementById('prodDesc')
+
+
+// var productlist=[]
+// if(localStorage.getItem('products')!=null){
+//     productlist=JSON.parse(localStorage.getItem('products'))
+//     displayproducts()
+// }
+
+// function getInputsData(){
+//     AddProduct()
+//     displayproducts()
+//     clear()
+// }
+
+// function AddProduct(){
+//         var product = {
+//         name: productNameInput.value,
+//         category: productCategoryInput.value,
+//         price: productPriceInput.value,
+//         description: productDescInput.value,
+//         }
+//         productlist.push(product)
+//     localStorage.setItem('products',JSON.stringify(productlist))
+// }
+
+// function displayproducts(){
+//     var cartona=''
+//     for(var i=0;i<productlist.length;i++){
+//     cartona +=`
+//     <tr>
+//     <td>${i}</td>
+//     <td>${productlist[i].name}</td>
+//     <td>${productlist[i].category}</td>
+//     <td>${productlist[i].price}</td>
+//     <td>${productlist[i].description}</td>
+//     <td><button onclick="deletItem(${i})" class="btn btn-danger">delet</button></td>
+//     <td><button class="btn btn-warning">update</button></td>
+//     </tr>
+//     `
+//     }
+//     document.getElementById('tbody').innerHTML =cartona
+// }
+
+// function clear(){
+//     productNameInput.value=""
+//     productCategoryInput.value=""
+//     productPriceInput.value=""
+//     productDescInput.value=""
+// }
+
+// function deletItem(index){
+//     productlist.splice(index, 1)
+//     localStorage.setItem('products',JSON.stringify(productlist))
+//     displayproducts()
+// }
+
+// function search(value){
+//     var cartona=''
+//     for(var i=0; i<prodlist.legth; i++){
+//     if(prodlist[i].name.toLowerCase().includes(value.toLowerCase())){
+//         cartona +=`
+//         <tr>
+//                 <td>${i}</td>
+//                 <td>${productlist[i].name}</td>
+//                 <td>${productlist[i].category}</td>
+//                 <td>${productlist[i].price}</td>
+//                 <td>${productlist[i].description}</td>
+//                 <td><button onklik="daletItme(${i})" class="btn btn-danger">delet</button></td>
+//                 <td><button class="btn bnt-warning">update</button></td>
+//             </tr>
+//             `
+//         }
+//         document.getElementById('tbody').innerHTML =cartona
+//     }
+//     }
+
+
+
+$(".openNav").click(function(){
+    $("#leftMenu").animate({ width:'250px'},50)
+   $("#home-content").animate({marginLeft :'250px'},50)
+})
+$(".closebtn").click(function(){
+    $("#leftMenu").animate({ width:'0px'},50)
+   $("#home-content").animate({marginLeft :'0px'},50)
+})
+$("#leftMenu a").click(function(){
+    var sectionId= $(this).attr("href");
+    var positionOfSection = $(sectionId).offset().top;
+    $("html , body").animate({scrollTop:positionOfSection},2000);
+})
+$('#sliderDown .toggle').click(function(){
+    $('.inner').not($(this).next()).slideUp(500);
+    $(this).next().slideToggle(500);
+});
+window.onload = function() {
+    countDownToTime("10 october 2021 9:56:00");
+}
+    function countDownToTime(countTo) {
+        let futureDate = new Date(countTo);
+    futureDate = (futureDate.getTime()/1000);
+    let now = new Date();
+    now = (now.getTime()/1000);
+    timeDifference = (futureDate- now);
+    let days = Math.floor( timeDifference / (24*60*60));
+   let hours = Math.floor((timeDifference - (days * (24*60*60))) / 3600);
+   let mins = Math.floor((timeDifference - (days * (24*60*60)) - (hours * 3600 )) / 60);
+   let secs = Math.floor((timeDifference - (days * (24*60*60)) - (hours * 3600) - (mins * 60)))
+    $(".days").html(`${days} D`);
+    $(".hours").html(`${hours} h`);
+    $(".minutes").html(`${ mins } m`);
+    $('.seconds').html(`${ secs} s`)
+    setInterval(function() {  countDownToTime(countTo); }, 1000);
+}
+/*textarea*/
+var maxLength = 100;
+$('textarea').keyup(function() {
+  var length = $(this).val().length;
+  var AmountLeft = maxLength-length;
+  if(AmountLeft<=0)
+            {
+                 $("#chars").text("your available character finished");
+                
+            }
+        else{
+        
+        $("#chars").text(AmountLeft);
+        }
+});
